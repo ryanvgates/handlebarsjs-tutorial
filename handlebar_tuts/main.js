4f2d91e4fc32c093042b8a020b8c545849f9db36
Ryan Gates <ryanvgates@gmail.com>
2016-03-21 16:32:10 -0400

    used handlebars.js for this project

--- handlebar_tuts/main.js
+++ handlebar_tuts/main.js
@@ -1,16 +1,13 @@
 $(function() {
 
 	var shoesData = [{name:"Nike", price:199.00 }, {name:"Loafers", price:59.00 }, {name:"Wing Tip", price:259.00 }];
-	
-	function updateAllShoes(shoes)  {
-		var theHTMLListOfShoes = "";
-		
-		shoesData.forEach (function (eachShoe)  { // Note the coupling and mixing of HTML and JavaScript; it is tedious to follow
-			theHTMLListOfShoes += '<li class="shoes">' + '<a href="/' + eachShoe.name.toLowerCase() + '">' + eachShoe.name + ' -- Price: ' + eachShoe.price + '</a></li>';
-		});
-		return theHTMLListOfShoes;
-	}
+	// Get the HTML from the template in the script tag
+	var theTemplateScript = $("#shoe-template").html();
 
-	$(".shoesNav").append (updateAllShoes(shoesData));
-	
+	// Compile the template
+	var theTemplate = Handlebars.compile(theTemplateScript);
+	$(".shoesNav").append(theTemplate(shoesData));
+
+	// We pass the shoesData object to the compiled handleBars function
+	// The function will insert all the values from the objects in their respective places in the HTML as a string. Then we use jQuery to append the resulting HTML string into the page
 });
