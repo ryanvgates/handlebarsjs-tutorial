a7ccb5e39ad33a82e4863844bd0bc7e7b0d92d8b
Ryan Gates <ryanvgates@gmail.com>
2016-03-21 16:51:04 -0400

    Added partials/sub-templates

--- handlebar_tuts/main.js
+++ handlebar_tuts/main.js
@@ -1,11 +1,13 @@
 $(function() {
 
-	var shoesData = [{name:"Nike", price:199.00 }, {name:"Loafers", price:59.00 }, {name:"Wing Tip", price:259.00 }];
+	var shoesData = {allShoes:[{name:"Nike", price:199.00, color:"black", size:10 }, {name:"Loafers", price:59.00, color:"blue", size:9 }, {name:"Wing Tip", price:259.00, color:"brown", size:11 }]};
+
 	// Get the HTML from the template in the script tag
 	var theTemplateScript = $("#shoe-template").html();
 
 	// Compile the template
 	var theTemplate = Handlebars.compile(theTemplateScript);
+	Handlebars.registerPartial("description", $("#shoe-description").html());
 	$(".shoesNav").append(theTemplate(shoesData));
 
 	// We pass the shoesData object to the compiled handleBars function
